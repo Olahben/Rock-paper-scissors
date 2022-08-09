49bf466439cd951fc26cdba3c4fb09d0955ca0ec
Ola <ohagenbendixen@gmail.com>
2022-08-09 13:41:04 +0200

    Write a function that plays a single round of rock paper scissors

--- script.js
+++ script.js
@@ -16,3 +16,29 @@ let computerPoints = 0;
 let playerPoints = 0;
 
 
+function playRound(playerSelection, computerSelection) { // function that plays a single round of Rock paper scissors
+    //playerSelection = playerSelection.toLowerCase();
+    if(playerSelection === computerSelection) {//Comparison of the computer selection and player selection
+        return 'Its a tie';
+    } else if(playerSelection === 'rock' && computerSelection === 'paper') {
+        computerPoints++//Add a point to the player who won that round
+        return 'You lost, rock does not beat paper'; // Return a statement based on that comparison
+    } else if(playerSelection === 'rock' && computerSelection === 'scissors') {
+        playerPoints++
+        return 'You won, rock beats scissors';
+    } else if(playerSelection === 'paper' && computerSelection === 'scissors') {
+        computerPoints++
+        return 'You lost, paper does not beat scissors';
+    } else if(playerSelection === 'paper' && computerSelection === 'rock') {
+        playerPoints++
+        return 'You won, paper beats rock';
+    } else if(playerSelection === 'scissors' && computerSelection === 'rock') {
+        computerPoints++ 
+        return 'You lost, scissors does not beat rock';
+    }else if(playerSelection === 'scissors' && computerSelection === 'paper') {
+        playerPoints++
+        return 'You won, scissors beats paper';
+    } else {
+        return 'A fatal error has ocurred'
+    }
+    } 
