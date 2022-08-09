62f0ef1df2f6a4ed4247c79723cfa3fc67fcb86f
Ola <ohagenbendixen@gmail.com>
2022-08-09 13:37:58 +0200

    Write the first function. The function returns rock, paper or scissors

---
+++ script.js
@@ -0,0 +1,13 @@
+//Return randomly either rock paper or scissors.
+function getComputerChoice() {
+    const randomNum = Math.floor(Math.random()* 99) + 1;
+    if(randomNum <= 33) {
+     return 'rock'
+    } else if(randomNum <= 66 && randomNum > 33) {
+     return 'paper'
+    } else {
+     return 'scissors'
+    }
+ }
+ 
+ //console.log(getComputerChoice());
